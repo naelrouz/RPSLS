e85e0407ae53ab4c065a61bee4d60264e77f8ae8
Nael Rouz <nael_rouz@mail.ru>
2018-05-24 22:44:17 +0300

    result coming only to 1 user

--- src/game/GameRoom.js
+++ src/game/GameRoom.js
@@ -5,122 +5,135 @@ import rules from './gameRules';
 import events from '../eventConstants';
 
 class GameRoom {
-    constructor(gameRoomId) {
-        this.$gameRoomId = gameRoomId;
-        this.$maxPlayersCount = 2;
-        this.$players = new Map();
+  constructor(gameRoomId) {
+    this.$gameRoomId = gameRoomId;
+    this.$maxPlayersCount = 2;
+    this.$players = new Map();
+  }
+  get id() {
+    return this.$gameRoomId;
+  }
+
+  get players() {
+    return this.$players;
+  }
+  get playersAsArray() {
+    return Array.from(this.$players.values());
+  }
+  addPlayer(socket) {
+    // TODO ? if such a player already exists 'This player already exists'
+
+    // if the maximum number of players is exceeded
+    if (this.$players.size === this.$maxPlayersCount) {
+      // throw new Error('Maximum number of players exceeded')
+      console.error('Maximum number of players exceeded');
+      return;
     }
-    get id() {
-        return this.$gameRoomId;
+
+    socket.join(this.$gameRoomId);
+    const newPlayer = new Player(socket);
+    this.$players.set(socket.id, newPlayer);
+
+    socket.broadcast
+      .in(this.$gameRoomId)
+      .emit(events.OPPONENT_ENTRANCE_TO_GAME_ROOM, {
+        message: `${socket.username} user has joined to GameRoom`
+      });
+  }
+  getPlayer(playerId) {
+    return this.$players.get(playerId);
+  }
+  isAllPlayersSelectedGestures() {
+    return Array.from(this.$players.values()).reduce((result, player) => {
+      if (!result) {
+        return result;
+      }
+
+      return !!player.selectedGesture;
+    }, true);
+  }
+  gameOwer(gameResult) {
+    // console.log('gameResult:', gameResult);
+
+    console.log('SOKET:', this.$players.values().next().value.$socket);
+
+    this.$players
+      .values()
+      .next()
+      .value.$socket.broadcast.in(this.$gameRoomId)
+      .emit(events.GAME_OWER, gameResult);
+  }
+  gameResult() {
+    const [player2, player1] = this.playersAsArray;
+
+    if (player1.selectedGesture === player2.selectedGesture) {
+      return {
+        message: '',
+        players: this.playersAsArray.map(player => ({
+          id: player.id,
+          winner: player.winner,
+          selectedGesture: player.selectedGesture
+        }))
+      };
     }
 
-    get players() {
-        return this.$players;
+    // if the player1 object contains the player2, the user wins
+    if (
+      rules[player1.selectedGesture].hasOwnProperty(player2.selectedGesture)
+    ) {
+      // return `player1! ${player1.selectedGesture}  ${rules[player1.selectedGesture][player2.selectedGesture]} ${player2.selectedGesture}`;
+
+      player1.$winner = true;
+
+      return {
+        message: `${player1.selectedGesture} ${
+          rules[player1.selectedGesture][player2.selectedGesture]
+        } ${player2.selectedGesture}`,
+        players: this.playersAsArray.map(player => ({
+          id: player.id,
+          winner: player.$winner,
+          selectedGesture: player.selectedGesture
+        }))
+      };
     }
-    get playersAsArray() {
-        return Array.from(this.$players.values());
+
+    player2.$winner = true;
+
+    return {
+      message: `${player2.selectedGesture}  ${
+        rules[player2.selectedGesture][player1.selectedGesture]
+      } ${player1.selectedGesture}`,
+      players: this.playersAsArray.map(player => ({
+        id: player.id,
+        winner: player.winner,
+        selectedGesture: player.selectedGesture
+      }))
+    };
+  }
+  // sendGameResults() {
+
+  // }
+  setPlayerGesture(playerId, gesture) {
+    // if the player has already chosen a gesture
+    if (this.$players.get(playerId).selectedGesture) {
+      return;
     }
-    addPlayer(socket) {
-        // TODO ? if such a player already exists 'This player already exists'
 
-        // if the maximum number of players is exceeded
-        if (this.$players.size === this.$maxPlayersCount) {
-            // throw new Error('Maximum number of players exceeded')
-            console.error('Maximum number of players exceeded');
-        }
+    this.$players.get(playerId).selectedGesture = gesture;
 
-        socket.join(this.$gameRoomId);
-        const newPlayer = new Player(socket);
-        this.$players.set(socket.id, newPlayer);
+    // if all players selected gestures
 
-        socket.broadcast
-            .in(this.$gameRoomId)
-            .emit(events.OPPONENT_ENTRANCE_TO_GAME_ROOM, {
-                message: `${socket.username} user has joined to GameRoom`
-            });
+    console.log(
+      'this.isAllPlayersSelectedGestures() : ',
+      this.isAllPlayersSelectedGestures()
+    );
+
+    if (this.isAllPlayersSelectedGestures()) {
+      const gameResult = this.gameResult();
+
+      this.gameOwer(gameResult);
     }
-    getPlayer(playerId) {
-        return this.$players.get(playerId);
-    }
-    isAllPlayersSelectedGestures() {
-        return Array.from(this.$players.values()).reduce((result, player) => {
-            if (!result) {
-                return result;
-            }
-
-            return !!player.selectedGesture;
-        }, true);
-    }
-    gameOwer(gameResult) {
-
-        const {
-            socket
-        } = gameResult.players[0];
-
-        socket.broadcast
-            .in(this.$gameRoomId)
-            .emit(events.GAME_OWER, gameResult);
-
-    }
-    gameResult() {
-        const [player2, player1] = this.playersAsArray;
-
-        if (player1.selectedGesture === player2.selectedGesture) {
-            return {
-                message: 'draw',
-                winner: null,
-                loser: null,
-                players: this.playersAsArray
-            };
-        }
-
-        // if the player1 object contains the player2, the user wins
-        if (
-            rules[player1.selectedGesture].hasOwnProperty(player2.selectedGesture)
-        ) {
-            // return `player1! ${player1.selectedGesture}  ${rules[player1.selectedGesture][player2.selectedGesture]} ${player2.selectedGesture}`;
-
-            player1.winner = true;
-
-            return {
-                message: `${player1.selectedGesture} ${rules[player1.selectedGesture][player2.selectedGesture]} ${player2.selectedGesture}`,
-                players: this.playersAsArray.map(player => ({
-                    [player.id]: {
-                        winner: player.winner,
-                        selectedGesture: player.selectedGesture
-                    }
-                }))
-            };
-        }
-
-        return `player2! ${player2.selectedGesture}  ${rules[player2.selectedGesture][player1.selectedGesture]} ${player1.selectedGesture}`;
-    }
-    // sendGameResults() {
-
-    // }
-    setPlayerGesture(playerId, gesture) {
-        // if the player has already chosen a gesture
-        if (this.$players.get(playerId).selectedGesture) {
-            return;
-        }
-
-        this.$players.get(playerId).selectedGesture = gesture;
-
-        // if all players selected gestures
-
-        console.log(
-            'this.isAllPlayersSelectedGestures() : ',
-            this.isAllPlayersSelectedGestures()
-        );
-
-        if (this.isAllPlayersSelectedGestures()) {
-            const gameResult = this.gameResult();
-
-            console.log('gameResult:', gameResult);
-
-            this.gameOwer(gameResult);
-        }
-    }
+  }
 }
 
 export default GameRoom;
--- src/game/GameServer.js
+++ src/game/GameServer.js
@@ -1,39 +1,41 @@
 import uniqid from 'uniqid';
 
-import GameRoom from './GameRoom'
+import GameRoom from './GameRoom';
 
 class GameServer {
   constructor() {
-    this.$gameRooms = new Map()
+    this.$gameRooms = new Map();
   }
   createGameRoom() {
     const gameRoomId = uniqid();
-    const newGameRoom = new GameRoom(gameRoomId)
-    this.$gameRooms.set(gameRoomId, newGameRoom)
-    return newGameRoom
+    const newGameRoom = new GameRoom(gameRoomId);
+    this.$gameRooms.set(gameRoomId, newGameRoom);
+    return newGameRoom;
   }
 
   getGameRoom(gameRoomId) {
-    return this.$gameRooms.get(gameRoomId)
+    return this.$gameRooms.get(gameRoomId);
   }
 
   getRoomPlayers(gameRoomId) {
-    return this.$gameRooms.get(gameRoomId).players
+    return this.$gameRooms.get(gameRoomId).players;
   }
 
   setRoomPlayerGesture(gameRoomId, playerId, gesture) {
-    this.$gameRooms.get(gameRoomId).setPlayerGesture(playerId, gesture)
+    this.$gameRooms.get(gameRoomId).setPlayerGesture(playerId, gesture);
   }
 
   addRoomPlayer(gameRoomId, socket) {
+    if (!this.getGameRoom(gameRoomId)) {
+      console.error('there is no room with such id');
+    }
 
-    this.getGameRoom(gameRoomId).addPlayer(socket)
+    this.getGameRoom(gameRoomId).addPlayer(socket);
   }
 
   get gameRooms() {
-    return this.$gameRooms
+    return this.$gameRooms;
   }
-
 }
 
 export default GameServer;
